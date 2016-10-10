6f809301d76db43dc6b2607a10a1010a85010b02
Fedor Plotnikov <blackdef@bk.ru>
2016-10-10 09:43:58 +0300

    Изменения настройки форматирования

--- sandbox/src/main/java/ru/astral/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/astral/test/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.astral.test.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args){
-		System.out.println("Hello, WORLD!!!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello, WORLD!!!");
+  }
 }
