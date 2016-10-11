d18be013bfd85d9dca7c35d59ffd5c71ddbaf049
Fedor Plotnikov <blackdef@bk.ru>
2016-10-11 12:58:05 +0300

    добавлено несколько простых функций

--- sandbox/src/main/java/ru/astral/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/astral/test/sandbox/MyFirstProgram.java
@@ -1,7 +1,28 @@
 package ru.astral.test.sandbox;
 
 public class MyFirstProgram {
+
+  public static void hello(String word) {
+    System.out.println("Hello, " + word +"!");
+  }
+  public static double area (double len){
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello, WORLD!!!");
+    hello("world");
+    hello("user");
+    hello("Fedor");
+    double l = 5.0;
+    double b = 4.0;
+    double a = 5.0;
+
+
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
   }
 }
