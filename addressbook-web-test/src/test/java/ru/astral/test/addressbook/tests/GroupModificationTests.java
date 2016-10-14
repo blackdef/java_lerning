f04c9022b241921db04ff0043e3e29eec0104f56
Fedor Plotnikov <blackdef@bk.ru>
2016-10-15 01:17:02 +0300

    Добавил тетст модификации групп

---
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,22 @@
+package ru.astral.test.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.astral.test.addressbook.model.GroupData;
+
+/**
+ * Created by Fedor on 15.10.2016.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test10", "test20", "test30"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+
+}
