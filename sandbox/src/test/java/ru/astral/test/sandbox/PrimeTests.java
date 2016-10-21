bc81378ea6f7a9bab42544a35ff1cbc5bd172469
Fedor Plotnikov <blackdef@bk.ru>
2016-10-21 15:11:38 +0300

    Работа с коллекциями

--- sandbox/src/test/java/ru/astral/test/sandbox/PrimeTests.java
+++ sandbox/src/test/java/ru/astral/test/sandbox/PrimeTests.java
@@ -16,4 +16,13 @@ public class PrimeTests {
   public void testNonPrime() {
     Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
   }
+  @Test(enabled = false)
+  public void testPrimeLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+  @Test
+  public void testPrimeFast() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
 }
