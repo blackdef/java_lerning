00687a317088964f67cd302bc659d9e36318ad3f
Fedor Plotnikov <blackdef@bk.ru>
2016-10-25 15:28:47 +0300

    Добавил проверку коллекции в модификацию Контакта. Тест падает !!!!
    Тест Падает ибо надо править выбор модифицируемого элмента. Там он идет
    не через галку а клик в картинку. причем если смотретьто изночально он
    то же не сильно правильно работал как я понял...

--- addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/ContactModificationTests.java
@@ -1,7 +1,11 @@
 package ru.astral.test.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.astral.test.addressbook.model.ContactData;
+
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * Created by Fedor on 17.10.2016.
@@ -11,9 +15,21 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification() {
     app.getNavigationHelper().gotoHomePage();
+    if(! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData("test1", "test2", null, "test4", "test5", "test6", "test7", "123456", "89876543210"));
+    }
+    List<ContactData> befor = app.getContactHelper().getContactList();
     app.getContactHelper().initModification();
-    app.getContactHelper().fillContactInfo(new ContactData("test10", "test20", "test30", "test40", "test50", "test60", "test70", "1234560", "898765432100"));
+    ContactData contact = new ContactData(befor.get(befor.size()-1).getId(),"test10", "test20", "test30", "test40", "test50", "test60", "test70", "1234560", "898765432100");
+    app.getContactHelper().fillContactInfo(contact);
     app.getContactHelper().submitContactUpdate();
     app.getContactHelper().returnHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(),befor.size());
+
+    befor.remove(befor.size()-1);
+    befor.add(contact);
+    Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(befor));
+
   }
 }
