5877e87d2df1f8ef35d7e65ce0edbe1ed321e4db
Fedor Plotnikov <blackdef@bk.ru>
2016-10-26 00:00:32 +0300

    Заменил цикл поиска макимума на лямду
    Ааааааа!!!!!!!! что это было ??????

--- addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/GroupCreateTest.java
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/GroupCreateTest.java
@@ -22,14 +22,7 @@ public class GroupCreateTest extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),befor.size() + 1);
 
-
-    int max = 0;
-    for (GroupData g : after){
-      if (max < g.getId()){
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     befor.add(group);
     Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(befor));
 
