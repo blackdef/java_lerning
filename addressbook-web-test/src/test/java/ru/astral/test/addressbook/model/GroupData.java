f36b558c8556528073872ae782aa78dee247357b
Fedor Plotnikov <blackdef@bk.ru>
2016-10-25 22:16:25 +0300

    Поправил тест создания в плане добавления проверки

--- addressbook-web-test/src/test/java/ru/astral/test/addressbook/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/model/GroupData.java
@@ -1,53 +1,27 @@
 package ru.astral.test.addressbook.model;
 
 public class GroupData {
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
-  private final String id;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
-    return id != null ? id.equals(groupData.id) : groupData.id == null;
-
-  }
-
-  @Override
-  public int hashCode() {
-    int result = name != null ? name.hashCode() : 0;
-    result = 31 * result + (id != null ? id.hashCode() : 0);
-    return result;
-  }
-
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            ", id='" + id + '\'' +
-            '}';
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
+
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -62,4 +36,33 @@ public class GroupData {
     return footer;
   }
 
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            '}';
+  }
 }
--- addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/GroupCreateTest.java
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/GroupCreateTest.java
@@ -3,8 +3,11 @@ package ru.astral.test.addressbook.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.astral.test.addressbook.model.ContactData;
 import ru.astral.test.addressbook.model.GroupData;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -14,10 +17,22 @@ public class GroupCreateTest extends TestBase {
   public void testGroupCreate() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> befor = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test2"));
+    GroupData group = new GroupData("test1", "test2", "test2");
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
-
     Assert.assertEquals(after.size(),befor.size() + 1);
+
+
+    int max = 0;
+    for (GroupData g : after){
+      if (max < g.getId()){
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    befor.add(group);
+    Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(befor));
+
   }
 
 
