f0b972cd6ace5c2c9cf10c1a995e77269d2293e4
Fedor Plotnikov <blackdef@bk.ru>
2016-11-07 09:30:49 +0300

    добавил библиотеку работы с параметрами запуска. Поправил запуск генератора с параметрами.

--- addressbook-web-test/src/test/java/ru/astral/test/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.astral.test.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.astral.test.addressbook.model.GroupData;
 
 import java.io.File;
@@ -7,22 +10,39 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
  * Created by Fedor on 06.11.2016.
  */
 public class GroupDataGenerator {
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
 
+  @Parameter(names = "-f", description = "Target file name")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex){
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
     List<GroupData> groups =  generatorGroups(count);
-    save(groups, file);
+    save(groups, new File(file));
 
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+
+  private void save(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups){
       writer.write(String.format("%s;%s;%s\n", group.getName(),group.getHeader(),group.getFooter()));
@@ -31,7 +51,7 @@ public class GroupDataGenerator {
 
   }
 
-  private static List<GroupData> generatorGroups(int count) {
+  private List<GroupData> generatorGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++)
     {
