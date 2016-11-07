0e98d72bc8bd1c7877a3a158d6557bdd333dd84d
blackdef <blackdef@bk.ru>
2016-11-07 16:00:31 +0300

    Добавлено чтение тестовых данных из CSV. Подключена библиотека для сериализации данных. Исправлен генератор тестовых данных

--- addressbook-web-test/src/test/java/ru/astral/test/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,7 @@ package ru.astral.test.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.astral.test.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,7 +11,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -22,6 +22,9 @@ public class GroupDataGenerator {
 
   @Parameter(names = "-f", description = "Target file name")
   public String file;
+
+  @Parameter(names = "-d", description = "Date format")
+  public String format;
 
   public static void main(String[] args) throws IOException {
     GroupDataGenerator generator = new GroupDataGenerator();
@@ -37,12 +40,29 @@ public class GroupDataGenerator {
 
   private void run() throws IOException {
     List<GroupData> groups =  generatorGroups(count);
-    save(groups, new File(file));
+    if (format.equals("csv")) {
+      saveAsCsv(groups, new File(file));
+    }else if (format.equals("xml")){
+      saveAsXml(groups, new File(file));
+    } else {
+      System.out.println("Unrecognized format " + format);
+    }
+
+  }
+
+  private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+    // http://x-stream.github.io/tutorial.html
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    String xml = xstream.toXML(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
 
   }
 
 
-  private void save(List<GroupData> groups, File file) throws IOException {
+  private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups){
       writer.write(String.format("%s;%s;%s\n", group.getName(),group.getHeader(),group.getFooter()));
