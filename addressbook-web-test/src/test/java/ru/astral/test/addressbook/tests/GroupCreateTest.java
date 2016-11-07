ce57048e29ac11926db04139d0aa14596f9a5adc
Fedor Plotnikov <blackdef@bk.ru>
2016-11-07 09:49:13 +0300

    Параметризировал запуск теста создания групп

--- addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/GroupCreateTest.java
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/GroupCreateTest.java
@@ -2,12 +2,16 @@ package ru.astral.test.addressbook.tests;
 
 
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.astral.test.addressbook.model.GroupData;
 import ru.astral.test.addressbook.model.Groups;
 
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,12 +19,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreateTest extends TestBase {
 
-  @Test(enabled = false)
-  public void testGroupCreate() {
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+    list.add(new Object[] {new GroupData().withName("test4").withHeader("header4").withFooter("footer4")});
+    return list.iterator();
+  }
+
+  @Test(enabled = true, dataProvider = "validGroups")
+  public void testGroupCreate(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-
-    GroupData group = new GroupData().withName("test1").withHeader("test2").withFooter("test2");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()+1));
     Groups after = app.group().all();
