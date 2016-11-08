7d777a54acbc78db488aefdb0899ba389e8265b0
blackdef <blackdef@bk.ru>
2016-11-08 19:35:03 +0300

    логирование в догонку изменения в  базовом классе тестов

--- addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/TestBase.java
@@ -1,26 +1,44 @@
 package ru.astral.test.addressbook.tests;
 
+import org.omg.CORBA.Object;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.*;
 import ru.astral.test.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by Fedor on 14.10.2016.
  */
 public class TestBase {
 
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
+
+  @BeforeMethod
+  public void logTestStart(Method m, java.lang.Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m){
+    logger.info(String.format("Stop test %s",m.getName()));
+  }
+
 
 }
