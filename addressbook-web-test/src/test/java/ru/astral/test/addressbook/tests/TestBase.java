5f0559100337be0877c1fde6d8aaf8b9d8d01688
blackdef <blackdef@bk.ru>
2016-11-08 18:53:51 +0300

    Добавил обработку параметров запуска тестов и чтение парамтеров из файла.

--- addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/astral/test/addressbook/tests/TestBase.java
@@ -12,7 +12,6 @@ import ru.astral.test.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeSuite
   public void setUp() throws Exception {
